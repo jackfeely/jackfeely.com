337c40e1ca7bed10b7bcd3863a3b6956f0259eaf
jackfeely <jack.feely@gmail.com>
2017-02-20 18:56:10 -0500

    make project divs clickable links

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  // $('#project-div').click(function(){
+  //   console.log($(this).attr('data-href'));
+  //   window.redirect = $(this).attr('data-href');
+  // })
+
+});
