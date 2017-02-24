1dec16d8eaf5e1ccb5e4a3771b783c41fccea3b8
jackfeely <jack.feely@gmail.com>
2017-02-24 12:42:57 -0500

    implement filter projects by skill functionality, further styling

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 $(document).ready(function(){
 
-  // $('#project-div').click(function(){
-  //   console.log($(this).attr('data-href'));
-  //   window.redirect = $(this).attr('data-href');
-  // })
+  var skillsFilter = [];
+  $('.skill-li').click(function(){
+    var skillLi = $(this),
+        skill = skillLi.text();
+    if (skillsFilter.includes(skill)) {
+      skillLi.removeClass('highlight-skill');
+      skillsFilter.splice(skillsFilter.indexOf(skill, 1));
+    } else {
+      skillLi.addClass('highlight-skill');
+      skillsFilter.push(skill);
+    }
+
+    displayFilteredProjects(skillsFilter);
+
+  });
+
+  function displayFilteredProjects(skillsFilter) {
+    $('.project-div').each(function(projectDiv){
+      var projectSkills = $(this).attr('skills').split(', ');
+
+      if (skillsFilter.length == 0 || skillsFilter.every(function(skill){
+        return projectSkills.indexOf(skill) >= 0;
+      })) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    }, this)
+  }
 
 });
